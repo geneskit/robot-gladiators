99b4218265899f4a480691906c4dd2287424085d
Pineapple Season <emsthai21@gmail.com>
2020-10-07 15:24:58 -0700

    updated fighting functions

--- assets/js/game.js
+++ assets/js/game.js
@@ -72,6 +72,8 @@ var fight = function(enemyName) {
     } 
 }
 
-for(var i = 0; i<enemyNames.length; i++) {
-    fight(enemyNames[i]);
+for(var i = 0; i < enemyNames.length; i++) {
+    var pickedEnemyName = enemyNames[i];
+    enemyHealth = 50;
+    fight(pickedEnemyName);
 }
