57041a359de0b246ebf198c8ce58eabfd403126d
Pineapple Season <emsthai21@gmail.com>
2020-10-07 18:26:10 -0700

    start and end game functions

--- assets/js/game.js
+++ assets/js/game.js
@@ -16,19 +16,16 @@ var enemyHealth = 50;
 var enemyAttack = 12;
 
 console.log(enemyNames.length);
+
 for(var i = 0; i < enemyNames.length; i++) {
     console.log(enemyNames[i]);
     console.log(i);
     console.log(enemyNames[i] + " is at " + i + " index");
   }
 
-
-
-window.alert("Welcome to Robot Gladiators!");
-
 var fight = function(enemyName) {
     
-    while (playerHealth > 0 && enemyHealth > 0) {}
+    while (playerHealth > 0 && enemyHealth > 0) {
 
         var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
 
@@ -72,8 +69,44 @@ var fight = function(enemyName) {
     } 
 }
 
-for(var i = 0; i < enemyNames.length; i++) {
-    var pickedEnemyName = enemyNames[i];
-    enemyHealth = 50;
-    fight(pickedEnemyName);
+var startGame = function() {
+    playerHealth = 100;
+    playerAttack = 10;
+    playerMoney = 10;
+
+    for (var i = 0; o < enemyNames.length; i++) {
+        if (playerHealth > 0) {
+            window.alert("Welcome to Robot Gladiators! Round: " + (i+1));
+            var pickedEnemyName = enemyNames[i];
+            enemyHealth = 50;
+            fight(pickedEnemyName);
+        }
+        else {
+            window.alert("You have lost your robot in battle! Game over!");
+            break;
+        }
+    }
+    endGame();
+}
+
+startGame();
+
+var endGame = function() {
+    window.alert("The game has now ended. Let's see how you did!");
+
+    if (playerHealth > 0) {
+        window.alert("Great job, you've survived the game! You know have the score of " + playerMoney + ".");
+    }
+    else {
+        window.alert("You've lost your robot in battle.");
+    }
+
+    var playAgainConfirm = window.confirm("Would you like to play again?");
+
+    if(playAgainConfirm) {
+        startGame();
+    }
+    else {
+        window.alert("Thank you for playing Robot Gladiators! Come back soon!");
+    }
 }
