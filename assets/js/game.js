abff65988bab59e6c38945ffc759e996a30693e9
Pineapple Season <emsthai21@gmail.com>
2020-10-08 22:46:33 -0700

    random health and attack values

--- assets/js/game.js
+++ assets/js/game.js
@@ -9,22 +9,20 @@ var playerHealth = 100;
 var playerAttack = 10;
 var playerMoney = 10;
 
-console.log(playerName, playerAttack, playerHealth);
-
 var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
-var enemyHealth = 50;
+var enemyHealth = randomNumber(40, 60);
 var enemyAttack = 12;
 
-console.log(enemyNames.length);
-
+/*
 for(var i = 0; i < enemyNames.length; i++) {
     console.log(enemyNames[i]);
     console.log(i);
     console.log(enemyNames[i] + " is at " + i + " index");
-  }
+}
+*/
 
 var fight = function(enemyName) {
-    
+       
     while (playerHealth > 0 && enemyHealth > 0) {
 
         var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
@@ -34,13 +32,16 @@ var fight = function(enemyName) {
 
             if (confirmSkip) {
                 window.alert(playerName + ' has decided to skip this fight. Goodbye!');
-                playerMoney = playerMoney - 10;
+                playerMoney = Math.max(0, playerMoney - 10);
                 console.log("playerMoney", playerMoney);
                 break;
             }
         }
 
-        enemyHealth = enemyHealth - playerAttack;
+        var damage = randomNumber(playerAttack - 3, playerAttack);
+
+        enemyHealth = Math.max(0, enemyHealth - damage);
+
         console.log(
             playerName + ' attacked' + enemyName + '. ' + enemyName + ' now has ' + enemyHealth + ' health remaining '
         );
@@ -54,7 +55,10 @@ var fight = function(enemyName) {
             window.alert(enemyName + " still has " + enemyHealth + " health left.")
         }
         
-        playerHealth = playerHealth - enemyAttack;
+        var damage = randomNumber(enemyAttack - 3, enemyAttack);
+
+        playerHealth = Math.max(0, playerHealth - damage);
+
         console.log(
             enemyName + ' attacked ' + playerName + '. ' + playerName + ' now has ' + playerHealth + ' health remaining.'
         );
@@ -74,11 +78,11 @@ var startGame = function() {
     playerAttack = 10;
     playerMoney = 10;
 
-    for (var i = 0; o < enemyNames.length; i++) {
+    for (var i = 0; i < enemyNames.length; i++) {
         if (playerHealth > 0) {
             window.alert("Welcome to Robot Gladiators! Round: " + (i+1));
             var pickedEnemyName = enemyNames[i];
-            enemyHealth = 50;
+            enemyHealth = randomNumber(40, 60);
             fight(pickedEnemyName);
             if (playerHealth > 0 && i < enemyNames.length - 1) {
                 var storeConfirm = window.confirm("The fight is over, visit the store before the next round?");
@@ -103,10 +107,10 @@ var shop = function() {
     switch (shopOptionPrompt) {
         case "REFILL":
         case "refill":
-            window.alert("Refilling player's health by 20 for 7 dollars.");
             if (playerMoney >= 7) {
+                window.alert("Refilling player's health by 20 for 7 dollars.");
                 playerHealth = playerHealth + 20;
-                playerMoney = playerMoney - 7;
+                playerMoney = Math.max(0, playerMoney - 10);
             }
             else {
                 window.alert("You don't have enough money!");
@@ -117,7 +121,7 @@ var shop = function() {
             if (playerMoney >= 7) {
                 window.alert("Upgrading player's attack by 6 for 7 dollars.");
                 playerAttack = playerAttack + 6;
-                playerMoney = playerMoney - 7;
+                playerMoney = Math.max(0, playerMoney - 7);
             }
             else {
                 window.alert("You don't have enough!");
@@ -152,6 +156,12 @@ var endGame = function() {
     else {
         window.alert("Thank you for playing Robot Gladiators! Come back soon!");
     }
+}
+
+var randomNumber = function(min, max) {
+    var value = Math.floor(Math.random() * (max - min + 1) + min);
+
+    return value;
 }
 
 startGame();
